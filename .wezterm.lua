6db7bae81cf691252f497933768d5204dae4fdc4
Loris De Biasi <loris.debiasi@unige.ch>
2024-10-08 13:36:25 +0200

    wezterm order

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,11 +2,11 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- CONFIG
+config.color_scheme = 'Catppuccin Mocha'
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 14
 config.enable_tab_bar = false
 config.initial_cols = 120
 config.initial_rows = 30
-config.color_scheme = 'Catppuccin Mocha'
 
 return config
