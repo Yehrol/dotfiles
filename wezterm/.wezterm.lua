7ed930ff316fc42d480a4b65fc9abd1843eb0f15
Loris De Biasi <loris.debiasi@gmail.com>
2025-01-15 11:25:30 +0100

    fixing r alias and wezterm beep

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,11 +2,12 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- CONFIG
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 14
 config.enable_tab_bar = false
 config.initial_cols = 120
 config.initial_rows = 30
+config.audible_bell = "Disabled"
 
 return config
