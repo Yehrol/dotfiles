1ad15e496733c22831fecd7b702a17ae52749e45
Loris De Biasi <loris.debiasi@gmail.com>
2025-02-27 14:08:31 +0100

    adding alias ys to search and a draft for a setup script

---
+++ setupArch.sh
@@ -0,0 +1,16 @@
+# draft for a setup script. Should check if the packages are already installed,
+# the os and add arguments select what to install
+
+# Oh My Posh
+curl -s https://ohmyposh.dev/install.sh | bash -s
+
+# ZSH
+sudo pacman -S zsh
+sudo pacman -S zsh-completions
+chsh -s $(which zsh)
+
+# GNU Stow
+sudo pacman -S stow
+
+# wezterm
+# maybe later
