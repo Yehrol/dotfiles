8d8e13a5413120070428dc7edd4246c3f4eacb02
Loris De Biasi <loris.debiasi@gmail.com>
2025-03-20 19:02:17 +0100

    fixing font

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,7 +3,7 @@ local config = wezterm.config_builder()
 
 -- CONFIG
 config.color_scheme = "Catppuccin Mocha"
-config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 14
 config.enable_tab_bar = false
 config.initial_cols = 120
