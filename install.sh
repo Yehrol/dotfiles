47ebf05e1126b6efee23df11166a4b46a959b3c5
Loris De Biasi <loris.debiasi@gmail.com>
2026-08-11 08:30:28 +0200

    adding install.sh

--- install.sh
+++ install.sh
@@ -1,37 +1,120 @@
-# draft for a setup script.
-# Should check if the packages are already installed, the os and add arguments
-# to select what to install
+# TODO check https://github.com/cebem1nt/dotfiles/blob/main/install.sh
 
-# YAY
-sudo pacman -S --needed git base-devel
+SRC=$(dirname "$0")
+LOG_FILE="install.log"
+
+_info() { printf '\033[32m[INFO]\033[0m %s\n' "$*"; }
+_warning() { printf '\033[33m[WARNING]\033[0m %s\n' "$*"; }
+_error() { printf '\033[31m[ERROR]\033[0m %s\n' "$*"; }
+
+prompt() {
+    read -r -p "$1"$'\033[32m [Y/n] \033[0m' CHOICE
+    if [ -z "$CHOICE" ]; then 
+        echo 1 
+    else
+        case "$CHOICE" in 
+            n|N|No|no ) echo 0;;
+            y|Y|* ) echo 1;;
+        esac
+    fi
+}
+
+check_packges() {
+    while IFS= read -r LINE; do
+        if [[ -z "$LINE" ]] || [[ ${LINE# } == \#* ]]; then 
+            continue
+        fi
+
+        PKG="${LINE//[[:space:]]/}"
+
+        if ! pacman -Q "$PKG" >/dev/null 2>&1 ; then
+            echo -n "$PKG "
+        fi
+    done < $1
+}
+
+verify_packages() {
+    _info "Verifying necessary packages...."
+
+    if which pacman >/dev/null 2>&1; then
+		# TODO check if files exist
+        MISSING=$(check_packges "$SRC/packages.txt") 
+        MISSING_AUR=$(check_packges "$SRC/packages.aur.txt") 
+
+        if [[ -n "$MISSING" ]]; then
+            _warning "The following packages are not installed:"
+            echo "    $MISSING"
+            _warning "If you know what you're doing, skip"
+
+            if [[ $(prompt "Install missing packages?") == 1 ]]; then
+                sudo pacman -Syu $MISSING
+            fi
+		else
+			_info "Everything is already installed"
+        fi
+
+        if [[ -n "$MISSING_AUR" ]]; then
+            _warning "The following AUR packages are not installed:"
+            echo "    $MISSING_AUR"
+            _warning "If you know what you're doing, skip"
+
+            if [[ $(prompt "Install missing packages?") == 1 ]]; then
+                if which yay >/dev/null 2>&1; then
+                    yay -S $MISSING_AUR
+                elif which paru >/dev/null 2>&1; then
+                    paru -S $MISSING_AUR
+                else
+                    _error "No AUR helper found, could not install."
+                    read -r -p "Press ENTER to continue..."
+                fi
+            fi
+        fi
+    else
+        _warning "You're not using an arch/arch based distro. This script wont work"
+        read -r -p "Press ENTER to continue..."
+    fi
+}
+
+# ============================================================================ #
+#                              INSTALL REQUIREMENT                             #
+# ============================================================================ #
+sudo pacman -Syu --needed git base-devel stow
+
+# ============================================================================ #
+#                                    INSTALL                                   #
+# ============================================================================ #
+
+# TODO if a step is canceled, everything should be (ctrl+c)
+
+## Yay
+# TODO check if already done
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-# Oh My Posh
+## Oh my posh
+# TODO check if already done
 curl -s https://ohmyposh.dev/install.sh | bash -s
-oh-my-posh font install JetBrainsMono
 
-# ZSH
-sudo pacman -S zsh
-#sudo pacman -S zsh-completions # shouldnt be needed
+## Everything else
+verify_packages | tee -a $LOG_FILE
+
+
+# ============================================================================ #
+#                                     SETUP                                    #
+# ============================================================================ #
+
+## ZSH
+# TODO check if already done
 chsh -s $(which zsh)
 
-# GNU Stow
-sudo pacman -S stow
+## Oh my posh
+# TODO check if already done
+oh-my-posh font install JetBrainsMono
+ 
 
-# FZF
-sudo pacman -S fzf
-
-# TODO: list things
-# alacritty
-# hyprland
-# mpv
-# thunar
-# thunar_archive_plugin # TODO change
-# file-roller # TODO change
-# ...
+# TODO add a message to say what to do afterward. config nextcloud, retrieve ssh key, keepassxc, etc.. 
 
 # things to install and setup
-# rclone
-# nextcloud
+# nextcloud # gui only ?
+# rclone # Desktop only
